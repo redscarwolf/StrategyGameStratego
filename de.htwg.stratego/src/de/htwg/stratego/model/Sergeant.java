a66ce4ec0bfc5809e31ccdba55846fe58a9fc7db
redscarwolf <kieser.ma@gmail.com>
2015-10-23 13:01:04 +0200

    updated Character and tests

--- de.htwg.stratego/src/de/htwg/stratego/model/Sergeant.java
+++ de.htwg.stratego/src/de/htwg/stratego/model/Sergeant.java
@@ -2,9 +2,7 @@ package de.htwg.stratego.model;
 
 public class Sergeant extends AbstractCharacter {
 
-	@Override
-	public boolean move(int x, int y) {
-		return false;
+	public Sergeant() {
+		super(SERGEANT_RANG, true);
 	}
-
 }
