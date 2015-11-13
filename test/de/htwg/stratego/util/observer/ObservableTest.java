2aed83957cf5796fd934eda4acac7d6989b63ccd
redscarwolf <kieser.ma@gmail.com>
2015-11-13 14:47:53 +0100

    ObserverTest finished

--- test/de/htwg/stratego/util/observer/ObservableTest.java
+++ test/de/htwg/stratego/util/observer/ObservableTest.java
@@ -1,33 +1,40 @@
 package de.htwg.stratego.util.observer;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import de.htwg.stratego.aview.tui.TextUI;
-import de.htwg.stratego.controller.StrategoController;
 import junit.framework.TestCase;
 
 public class ObservableTest extends TestCase {
-	private IObserver observer;
-	private List<IObserver> myObservers = new ArrayList<>();
+	private boolean ping = false;
+	private TestObserver testObserver;
+	private Observable observable;
+	
+	class TestObserver implements IObserver {
+		//@Override
+		public void update(Event e) {
+			ping = true;
+		}
+	}
 	
 	@BeforeClass
 	public void setUp() {
-		observer = new TextUI(new StrategoController(2,3));
+		testObserver = new TestObserver();
+		observable = new Observable();
+		observable.addObserver(testObserver);
 	}
 	
 	@Test
-	public void testGetWidth() {
-		assertEquals(0,0);
+	public void testNotifyObservers() {
+		assertFalse(ping);
+		observable.notifyObservers();
+		assertTrue(ping);
 	}
 	
 	@Test
-	public void TestAddObserver() {
-//		assertEquals(0,0);
-		myObservers.add(observer);
-		assertEquals(myObservers.get(0),observer);
+	public void testRemoveObserver() {
+		assertFalse(ping);
+		observable.removeObserver(testObserver);
+		observable.notifyObservers();
+		assertFalse(ping);
 	}
 }
