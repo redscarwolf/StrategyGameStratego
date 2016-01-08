035acaef0d08a2f50c2757a4287d768109d9e3fa
redscarwolf <kieser.ma@gmail.com>
2016-01-08 16:17:13 +0100

    logging

--- src/de/htwg/stratego/aview/tui/TextUI.java
+++ src/de/htwg/stratego/aview/tui/TextUI.java
@@ -2,6 +2,9 @@ package de.htwg.stratego.aview.tui;
 
 import java.util.regex.Pattern;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import de.htwg.stratego.controller.IStrategoController;
 import de.htwg.stratego.util.observer.Event;
 import de.htwg.stratego.util.observer.IObserver;
@@ -9,6 +12,7 @@ import de.htwg.stratego.util.observer.IObserver;
 public class TextUI implements IObserver {
 
 	private IStrategoController sc;
+	private Logger logger = LogManager.getLogger(TextUI.class.getName());
 
 	public TextUI(IStrategoController sc) {
 		this.sc = sc;
@@ -25,7 +29,7 @@ public class TextUI implements IObserver {
 
 		boolean correctInput = Pattern.matches("q|h|f|(a \\d \\d \\d\\d?)|(r \\d \\d)|(m \\d \\d \\d \\d)", input);
 		if (!correctInput) {
-			System.out.println("Illegal command!");
+			logger.info("Illegal command!");
 			return continu;
 		}
 
@@ -53,7 +57,7 @@ public class TextUI implements IObserver {
 					Integer.valueOf(inputStrings[3]), Integer.valueOf(inputStrings[4]));
 			break;
 		default:
-			System.out.println("Illegal command!");
+			logger.info("Illegal command!");
 		}
 
 		return continu;
@@ -69,17 +73,17 @@ public class TextUI implements IObserver {
 		sb.append("m [x1] [y1] [x2] [y2] - Moves a character from (x1,y1) to (x2,y2)\n");
 		sb.append("f - Finish your construction phase\n");
 		sb.append("q - Quit game\n");
-		System.out.println(sb);
-		System.out.print("Command (Type \"h\" for help): ");
+		logger.info(sb);
+		logger.info("Command (Type \"h\" for help): ");
 	}
 
 	public void printTUI() {
-		System.out.println(sc.getStatusString());
-		System.out.println(sc.getPlayerStatusString());
-		System.out.print(sc.getFieldString());
-		System.out.println("Characters Player1: " + sc.getCharacterListString(sc.getPlayerOne()));
-		System.out.println("Characters Player2: " + sc.getCharacterListString(sc.getPlayerTwo()));
-		System.out.print("Command (Type \"h\" for help): ");
+		logger.info(sc.getStatusString());
+		logger.info(sc.getPlayerStatusString());
+		logger.info("\n" + sc.getFieldString());
+		logger.info("Characters Player1: " + sc.getCharacterListString(sc.getPlayerOne()));
+		logger.info("Characters Player2: " + sc.getCharacterListString(sc.getPlayerTwo()));
+		logger.info("Command (Type \"h\" for help): ");
 	}
 
 }
